e3ec7b5ad7e4fcb431acc516c0cafd68a59a3f08
Sad Pencil <qh06@qq.com>
2023-04-08 01:16:07 +0800

    revert print stack trace in solThrow

--- libsolutil/Exceptions.h
+++ libsolutil/Exceptions.h
@@ -52,8 +52,7 @@ struct Exception: virtual std::exception, virtual boost::exception
 		::solidity::util::errinfo_comment((_description)) << \
 		::boost::throw_function(ETH_FUNC) << \
 		::boost::throw_file(__FILE__) << \
-		::boost::throw_line(__LINE__) <<             \
-	    ::solidity::util::errinfo_comment(::boost::stacktrace::stacktrace()) \
+		::boost::throw_line(__LINE__) \
 	)
 
 /// Defines an exception type that's meant to signal a specific condition and be caught rather than
